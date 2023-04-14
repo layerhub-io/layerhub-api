b546968764f3f0302ca93769f9455b9b51eec7ef
Dany Boza <db@backium.co>
2023-04-14 10:10:50 -0400

    add component routes

--- src/routes.ts
+++ src/routes.ts
@@ -10,20 +10,30 @@ const setupRoutes = ({
   services: Services;
 }) => {
   const router = Router();
+
+  // Auth
   router.post('/auth/signup', handlers.users.signup.bind(handlers.users));
   router.post('/auth/signin', handlers.users.signin.bind(handlers.users));
   router.post('/auth/verify', handlers.users.signup);
   router.post('/auth/me', handlers.users.signup);
 
+  // Fonts
   router.get('/fonts', handlers.fonts.list.bind(handlers.fonts)); // editor fonts
   router.post('/fonts/_search', handlers.fonts.search.bind(handlers.fonts));
 
+  // Designs
   router.post(
     '/designs',
     checkJwt,
     handlers.designs.create.bind(handlers.designs)
   );
   router.get('/designs', handlers.designs.list.bind(handlers.designs));
+
+  // Components
+
+  router.get('/components', (req, res) => {
+    res.send({ components: [] });
+  });
 
   return router;
 };
