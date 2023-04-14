c9067cca2f72b3e10c6843070f5eb98dbe4e6192
Dany Boza <db@backium.co>
2023-04-14 10:28:00 -0400

    update rate limit params

--- src/common/middleware/rate-limit.ts
+++ src/common/middleware/rate-limit.ts
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 export const rateLimiter = rateLimit({
   // windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
   windowMs: 10 * 60 * 1000, // 15 minutes
-  max: 5,
-  message: 'You have exceeded the 5 requests in 15m limit!',
+  max: 20,
+  message: 'You have exceeded the 20 requests in 15m limit!',
   headers: true
 });
